a890e801bcbf144a190d402e8c6c7fcf0a0c932f
Victor Silva <victor.silvapc@hotmail.com>
2023-12-01 23:50:07 -0300

    commiting

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -4,10 +4,12 @@ class TransactionModel {
     required this.date,
     required this.description,
     required this.value,
+    required this.type,
   });
 
   final String category;
   final double value;
   final String description;
   final DateTime date;
+  final String type;
 }
