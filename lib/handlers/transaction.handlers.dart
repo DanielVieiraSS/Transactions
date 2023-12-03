4a68780709eda564771eb0e987af286557337f14
Victor Silva <victor.silvapc@hotmail.com>
2023-12-02 21:33:44 -0300

    fix: :sparkles: fixing issues on create a new transaction

--- lib/handlers/transaction.handlers.dart
+++ lib/handlers/transaction.handlers.dart
@@ -1,28 +1,52 @@
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:expenses/constants.dart';
+import 'package:expenses/models/transaction_model.dart';
+import 'package:expenses/supabase/instance.dart';
 import 'package:intl/intl.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
-insertSupabase(
+Future<List<TransactionModel>> fetchTransactionsHandler() async {
+  List transactions = await supabase
+      .from('transactions')
+      .select("*")
+      .eq('userId', loggedUser!.id);
+
+  return transactions.map((transaction) {
+    return TransactionModel(
+      category: transaction['category'],
+      date: transaction['created_at'],
+      description: transaction['description'],
+      value: double.parse(transaction['price'].toString()),
+      type: transaction['type'],
+    );
+  }).toList();
+}
+
+Future<TransactionModel> saveTransactionHandler(
   String description,
   double price,
   String category,
   String userId,
   String type,
 ) async {
-  final supabase = Supabase.instance.client;
-  String dateFormatted = DateFormat('dd/MM/yyyy').format(
-    DateTime.now(),
-  );
+  String dateFormatted = DateFormat('dd/MM/yyyy').format(DateTime.now());
 
-  await supabase.from('transactions').insert(
-    {
-      'created_at': dateFormatted,
-      "description": description,
-      "price": price,
-      "category": category,
-      "userId": userId,
-      'type': type,
-    },
+  Map<String, dynamic> payload = {
+    "userId": userId,
+    "description": description,
+    "price": price,
+    "category": category,
+    'type': type,
+    'created_at': dateFormatted,
+  };
+
+  await supabase.from('transactions').insert(payload);
+
+  return TransactionModel(
+    category: category,
+    date: dateFormatted,
+    description: description,
+    value: price,
+    type: type,
   );
 }
